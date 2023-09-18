a367df505b540d39fa682f4569b9eade9191c15d
= <=>
2023-09-18 13:25:31 +0200

    testvsc(simon)

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
 <div>
   <nav>
+    <div class='logo'>
     
+    </div>
+    <ul>
+      <li>Meny</li>
+      <li>Om oss</li>
+      <li>Kontakta oss</li>
+    </ul>
   </nav>
   <main></main>
   <footer></footer>
