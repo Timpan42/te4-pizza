e1f8a39bbc2ecc0a972b6de5b7cc846046832e55
= <=>
2023-09-18 15:30:59 +0200

    fix(simon)

--- main.js
+++ main.js
@@ -349,24 +349,6 @@ document.querySelector('#app').innerHTML = `
       </ul>
     </div>
     <div class="contact-container"></div>
-=======
-    <div class="top-container">
-      <h1>Smack av Ume!</h1>
-      <h2>Måndag-Tordag: 11-20 | Fredag-Söndag: 12-02 </h2>
-      <h3>099-09-pizza</h3>
-    </div>
-    <div>
-
-    </div>
-
-    <div class="menu-container">
-
-    </div>
-
-    <div class="contact-container">
-
-    </div>
->>>>>>> 692546f047b8ecc2a3f47d9f8461537c14b1bf9f
   </main>
   <footer>
 
