becde8c45b15b55166ca203610cda413e345a0ee
= <=>
2023-09-18 13:27:06 +0200

    💸 Börjat på navbar

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 import './style.css'
+import './nav.css'
 
 document.querySelector('#app').innerHTML = `
 <div>
   <nav>
-    
+    <div class="nav-logo">
+      <h1>UMEPIZZA</h1>
+    </div>
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a href="">Meny</a>
+      </li>
+      <li class="nav-item">
+        <a href="">Kontakt</a>
+      </li>
+      <li class="nav-item">
+        <a href="">Om Oss</a>
+      </li>
+    </ul>
   </nav>
   <main></main>
   <footer></footer>
