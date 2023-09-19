f5fd804e5b5c040f11b483a7412136f28c946553
= <=>
2023-09-19 08:40:45 +0200

    Navbar fix (Rasmus)

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ document.querySelector('#app').innerHTML = `
 
     <div class="navbar">
       <div class="nav-logo">
-        <h1>UMEPIZZA</h1>
+        <a>UMEPIZZA</a>
       </div>
       <button class="nav-menu">Menu</button>
     </div>
@@ -359,6 +359,11 @@ document.querySelector('#app').innerHTML = `
         <h1>Kontakt information</h1>
         <p>fake address 17, Umeå</p>
         <p>099-09-pizza</p>
+        <p>Photo by <a
+            href="https://unsplash.com/@iavnt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ivan
+            Torres</a> on <a
+            href="https://unsplash.com/photos/MQUqbmszGGM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        </p>
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7257.341272498429!2d20.208207204164115!3d63.79257890240078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5be90eceb9fd%3A0x26411d9c0060d007!2zU8O2ZHJhIFLDtmLDpGNrc3bDpGdlbiAzOCwgOTA0IDQwIFLDtmLDpGNr!5e1!3m2!1ssv!2sse!4v1695105122976!5m2!1ssv!2sse"
