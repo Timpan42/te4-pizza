ca7d1130852c6ac1e4ec5eabd7221481e1872c31
Timpan42 <tim@fagerdal.se>
2023-09-19 08:27:34 +0200

    css root saker

--- main.js
+++ main.js
@@ -4,21 +4,26 @@ import './nav.css'
 document.querySelector('#app').innerHTML = `
 <div>
   <nav>
+
     <div class="navbar">
       <div class="nav-logo">
         <h1>UMEPIZZA</h1>
       </div>
       <button class="nav-menu">Menu</button>
     </div>
+
     <ul class="nav-list collapse">
       <li class="nav-item">
         <a href="">Meny</a>
       </li>
+
       <li class="nav-item">
         <a href="">Kontakt</a>
       </li>
+
       <li class="nav-item">
         <a href="">Om Oss</a>
+
       </li>
     </ul>
   </nav>
@@ -31,6 +36,7 @@ document.querySelector('#app').innerHTML = `
     </div>
 
     <div class="menu-container">
+
       <ul class="menu">
         <li class="pizza">
           <ul class="ingredients">
@@ -346,8 +352,21 @@ document.querySelector('#app').innerHTML = `
           </ul>
         </li>
       </ul>
+
     </div>
-    <div class="contact-container"></div>
+    <div class="contact-container">
+      <div class="contact-text">
+        <h1>Kontakt information</h1>
+        <p>fake address 17, Umeå</p>
+        <p>099-09-pizza</p>
+      </div>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4568.832613320018!2d20.280790054612638!3d63.81112184762234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5babd2a50001%3A0x7a47960385db88a5!2sDermatologhuset!5e1!3m2!1ssv!2sse!4v1695103893288!5m2!1ssv!2sse"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
   </main>
   <footer>
 
