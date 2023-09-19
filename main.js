483ddf89d8b99494fc4fb9eecf56b151c0761545
Timpan42 <tim@fagerdal.se>
2023-09-19 08:32:33 +0200

    ny google map

--- main.js
+++ main.js
@@ -360,9 +360,8 @@ document.querySelector('#app').innerHTML = `
         <p>fake address 17, Umeå</p>
         <p>099-09-pizza</p>
       </div>
-
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4568.832613320018!2d20.280790054612638!3d63.81112184762234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5babd2a50001%3A0x7a47960385db88a5!2sDermatologhuset!5e1!3m2!1ssv!2sse!4v1695103893288!5m2!1ssv!2sse"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7257.341272498429!2d20.208207204164115!3d63.79257890240078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5be90eceb9fd%3A0x26411d9c0060d007!2zU8O2ZHJhIFLDtmLDpGNrc3bDpGdlbiAzOCwgOTA0IDQwIFLDtmLDpGNr!5e1!3m2!1ssv!2sse!4v1695105122976!5m2!1ssv!2sse"
         width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"></iframe>
 
