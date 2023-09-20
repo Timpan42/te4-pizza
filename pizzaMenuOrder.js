ded59a98c2562094917424333fe989713eecbed8
= <=>
2023-09-20 13:32:41 +0200

    Sak

--- pizzaMenuOrder.js
+++ pizzaMenuOrder.js
@@ -2,6 +2,5 @@ const pizzas = document.querySelectorAll('.pizza')
 let number = 1
 pizzas.forEach(pizza => {
     pizza.children[0].innerHTML = number + '. ' + pizza.children[0].innerHTML
-    console.log(pizza.children[0].innerHTML)
     number++
 })
